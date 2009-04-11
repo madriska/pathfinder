91583c1f019390489611b258ce56f6215c69ccf2
Brad Ediger <brad.ediger@madriska.com>
2009-04-10 21:42:50 -0500

    Removing dead code

--- pdf_test.rb
+++ pdf_test.rb
@@ -9,7 +9,6 @@ path = Path.new(Point.new(0,0), Point.new(100,100), map)
 pdf = map.to_pdf
 path = path.shortest_path
 path.draw(pdf) if path
-#path.draw_event_angles(pdf)
 pdf.render_file('out.pdf')
 
 `open out.pdf`
